52d0974d399fd20257c2dcfc6ad7bd82b5c44504
Weber Chen <weberchen@qnap.com>
2016-10-02 19:47:51 +0800

    [UI] Add recyclerview layout.

--- app/src/main/java/Activity/MainActivity.java
+++ app/src/main/java/Activity/MainActivity.java
@@ -1,52 +1,133 @@
 package activity;
 
-import android.annotation.TargetApi;
-import android.app.SearchManager;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
-import android.content.Intent;
-import android.database.MatrixCursor;
-import android.os.Build;
-import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 import com.example.weber.qsirch_offlinefiles.R;
 
+import adapter.DrawerAdapter;
+import fragment.FilesearchFragment;
+import fragment.HomeFragment;
+
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     public static String TAG = "MainActivity";
+
+    private Context context;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList; // 左邊選單List
+    private ActionBarDrawerToggle mDrawerToggle;
+
+    private CharSequence mDrawerTitle; // 抽屜 title
+    private CharSequence mTitle; // Activity title
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Get the intent, verify the action and get the query
-        Intent intent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-//            doMySearch(query);
+        context = this;
+        mTitle = mDrawerTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // 設定 mDrawerList's Adapter listener
+        mDrawerList.setAdapter(new ArrayAdapter<String>(context,
+                R.layout.item_drawer, DrawerAdapter.functionName));
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // 將導航抽屜選單作用在ActionBar上
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Remove the title text from the Android ActionBar
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // 設定 ActionBarDrawerToggle
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* 主要 Activity */
+                mDrawerLayout,         /* DrawerLayout xml */
+                R.string.drawer_open,  /* 開啓抽屜簡述 */
+                R.string.drawer_close  /* 關閉抽屜簡述 */
+        ) {
+            public void onDrawerClosed(View view) {
+                // 設定 action bar title
+                getSupportActionBar().setTitle(mTitle);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                // 設定 action bar title
+                getSupportActionBar().setTitle(mDrawerTitle);
+            }
+        };
+        // ActionBarDrawerToggle 狀態與 DrawerLayout 同步
+        mDrawerToggle.syncState();
+        // DrawerLayout設定DrawerToggle監聽器
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        // 預設顯示 position = 2 的資料內容
+        if (savedInstanceState == null) {
+            selectItem(0);
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.options_menu, menu);
-
-        // Associate searchable configuration with the SearchView
-        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
-        Log.d(TAG, "" + searchManager.getSearchableInfo(getComponentName()));
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-
-        return true;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // 打開/關閉抽屜與打開方向
+        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            mDrawerLayout.openDrawer(GravityCompat.START);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
+    // 抽屜項目列表監聽器
+    private class DrawerItemClickListener
+            implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view,
+                                int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int position) {
+        Fragment fragment = new HomeFragment();
+        switch (position) {
+            case 0:
+                fragment = new HomeFragment();
+                break;
+            case 1:
+                fragment = new FilesearchFragment();
+                break;
+        }
+        // 建立 Fragment
+
+        Bundle args = new Bundle();
+        args.putInt("position", position);
+        fragment.setArguments(args);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame,
+                fragment).commit();
+
+        // 設定是否要表現出項目被選定的樣式
+        mDrawerList.setItemChecked(position, true);
+        // 設定 Activity title
+        setTitle(DrawerAdapter.functionName[position]);
+        // 關閉抽屜
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+    }
 
     @Override
     public boolean onQueryTextSubmit(String s) {
--- app/src/main/java/Adapter/FileSearchAdapter.java
+++ app/src/main/java/Adapter/FileSearchAdapter.java
@@ -1,8 +1,76 @@
 package adapter;
 
+import android.content.Context;
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.weber.qsirch_offlinefiles.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.FileSearchModel;
+
 /**
  * Created by Weber on 2016/9/21.
  */
 
-public class FileSearchAdapter {
+
+public class FileSearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    ArrayList<? extends  FileSearchModel> data;
+    ArrayList<Integer> selectedPositions;
+    Context mContext;
+
+    public FileSearchAdapter(Context context, ArrayList<? extends FileSearchModel> objects) {
+        data = objects;
+        selectedPositions = new ArrayList<>();
+        mContext = context;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ListItemViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_offlinefile_list, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        FileSearchModel mFileSearchModel = data.get(position);
+        ListItemViewHolder mListItemViewHolder = (ListItemViewHolder) holder;
+        mListItemViewHolder.ItemTitle.setText(mFileSearchModel.getFileName());
+        mListItemViewHolder.ItemText.setText(mFileSearchModel.getFileDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListItemViewHolder extends RecyclerView.ViewHolder {
+        public View mView;
+        public TextView ItemTitle;
+        public TextView ItemSize;
+        public TextView ItemText;
+        public ImageView listImage;
+        public ImageView ItemImage;
+
+        public ListItemViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            ItemTitle = (TextView) itemView.findViewById(R.id.ItemTitle);
+            ItemSize = (TextView) itemView.findViewById(R.id.ItemSize);
+            ItemText = (TextView) itemView.findViewById(R.id.ItemText);
+            listImage = (ImageView) itemView.findViewById(R.id.listImage);
+            ItemImage = (ImageView) itemView.findViewById(R.id.ItemImage);
+        }
+    }
 }
+
+
