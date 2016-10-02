261ed797114741b95215197a9280401711080460
Weber Chen <weberchen@qnap.com>
2016-10-02 19:49:01 +0800

    [UI] Add recyclerview drawable.

---
+++ app/src/main/java/adapter/drawerAdapter.java
@@ -0,0 +1,16 @@
+package adapter;
+
+/**
+ * Created by Weber on 2016/9/26.
+ */
+
+public class DrawerAdapter {
+    public static String[] functionName;
+//    public static int[] functionId;
+
+    static {
+        functionName = new String[]{
+                "Home", "FileSearch"
+        };
+    }
+}
---
+++ app/src/main/java/model/FileSearchModel.java
@@ -0,0 +1,54 @@
+package model;
+
+/**
+ * Created by Weber on 2016/9/30.
+ */
+
+public class FileSearchModel {
+    String fileName;
+    String fileType;
+    String fileSize;
+    String fileDate;
+    String fileTime;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getFileDate() {
+        return fileDate;
+    }
+
+    public void setFileDate(String fileDate) {
+        this.fileDate = fileDate;
+    }
+
+    public String getFileTime() {
+        return fileTime;
+    }
+
+    public void setFileTime(String fileTime) {
+        this.fileTime = fileTime;
+    }
+}
+
