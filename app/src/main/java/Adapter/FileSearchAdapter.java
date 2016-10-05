7bda82eba02279e52e25efe0855739cfadd56006
Weber Chen <weberchen@qnap.com>
2016-10-05 16:46:44 +0800

    [UI] Add search file icon.

--- app/src/main/java/Adapter/FileSearchAdapter.java
+++ app/src/main/java/Adapter/FileSearchAdapter.java
@@ -3,7 +3,9 @@ package adapter;
 import android.content.Context;
 import android.media.Image;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
@@ -12,10 +14,18 @@ import android.widget.TextView;
 
 import com.example.weber.qsirch_offlinefiles.R;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import model.FileSearchModel;
+import utils.SimpleUtils;
+
+import static utils.SimpleUtils.formatCalculatedSize;
+import static utils.SimpleUtils.openFile;
 
 /**
  * Created by Weber on 2016/9/21.
@@ -23,34 +33,69 @@ import model.FileSearchModel;
 
 
 public class FileSearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-
-    ArrayList<? extends  FileSearchModel> data;
+    public static String TAG = "FileSearchAdapter";
+    ArrayList<? extends FileSearchModel> data;
     ArrayList<Integer> selectedPositions;
     Context mContext;
 
-    public FileSearchAdapter(Context context, ArrayList<? extends FileSearchModel> objects) {
-        data = objects;
+    public FileSearchAdapter(Context context) {
+        data = new ArrayList<>();
         selectedPositions = new ArrayList<>();
         mContext = context;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return new ListItemViewHolder(LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.item_offlinefile_list, parent, false));
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_offlinefile_list, parent, false);
+        final ListItemViewHolder vh = new ListItemViewHolder(view);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openFile(mContext, data.get(vh.getAdapterPosition()).getFileName());
+            }
+        });
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                view.requestFocus();
+                Log.d(TAG, "TOUCHED");
+                return false;
+            }
+        });
+        return vh;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        SimpleDateFormat DateFormatter = new SimpleDateFormat("yyyy/MM/dd");
+        SimpleDateFormat TimeFormatter = new SimpleDateFormat("hh:mm:ss");
         FileSearchModel mFileSearchModel = data.get(position);
         ListItemViewHolder mListItemViewHolder = (ListItemViewHolder) holder;
-        mListItemViewHolder.ItemTitle.setText(mFileSearchModel.getFileName());
-        mListItemViewHolder.ItemText.setText(mFileSearchModel.getFileDate());
+        mListItemViewHolder.ItemTitle.setText(mFileSearchModel.getFileName().toString().substring(mFileSearchModel.getFileName().toString().lastIndexOf("/") + 1));
+        mListItemViewHolder.ItemSize.setText(formatCalculatedSize(mFileSearchModel.getFileSize()));
+        mListItemViewHolder.ItemText.setText(DateFormatter.format(mFileSearchModel.getFileDate()));
+        mListItemViewHolder.ItemTime.setText(TimeFormatter.format(mFileSearchModel.getFileTime()));
     }
 
+    // If return 0, it will show nothing.
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
+    }
+
+    public void addContent(ArrayList<? extends FileSearchModel> files) {
+        if (!data.isEmpty())
+            data.clear();
+        data = files;
+        notifyDataSetChanged();
+    }
+
+    public void clearContent() {
+        data.clear();
+        notifyDataSetChanged();
+    }
+
+    private void sortContent(String sort, int order) {
     }
 
     public class ListItemViewHolder extends RecyclerView.ViewHolder {
@@ -58,6 +103,7 @@ public class FileSearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         public TextView ItemTitle;
         public TextView ItemSize;
         public TextView ItemText;
+        public TextView ItemTime;
         public ImageView listImage;
         public ImageView ItemImage;
 
@@ -67,10 +113,21 @@ public class FileSearchAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             ItemTitle = (TextView) itemView.findViewById(R.id.ItemTitle);
             ItemSize = (TextView) itemView.findViewById(R.id.ItemSize);
             ItemText = (TextView) itemView.findViewById(R.id.ItemText);
+            ItemTime = (TextView) itemView.findViewById(R.id.ItemTime);
             listImage = (ImageView) itemView.findViewById(R.id.listImage);
             ItemImage = (ImageView) itemView.findViewById(R.id.ItemImage);
         }
     }
+
+  public class CustomComparator implements Comparator<FileSearchModel> {
+      @Override
+      public int compare(FileSearchModel t1, FileSearchModel t2) {
+          if(t1.getFileName())
+          return 0;
+      }
+  }
+
+
 }
 
 
--- app/src/main/java/Fragment/FilesearchFragment.java
+++ app/src/main/java/Fragment/FilesearchFragment.java
@@ -1,10 +1,12 @@
 package fragment;
 
 import android.app.Fragment;
+import android.app.ProgressDialog;
 import android.app.SearchManager;
 import android.content.Context;
-import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -13,16 +15,19 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.weber.qsirch_offlinefiles.R;
 
+import java.io.File;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import adapter.FileSearchAdapter;
 import model.FileSearchModel;
+import utils.SimpleUtils;
 
 /**
  * Created by Weber on 2016/9/21.
@@ -35,24 +40,12 @@ public class FilesearchFragment extends Fragment {
     private RecyclerView mRecyclerView;
     private ArrayList<FileSearchModel> mDatas;
     private FileSearchAdapter mAdapter;
+    private LinearLayoutManager mLinearLayoutManager;
+    private Context context;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initData();
-    }
-
-    protected void initData() {
-        FileSearchModel dataModel = new FileSearchModel();
-        mDatas = new ArrayList<>();
-        dataModel.setFileName("File");
-        dataModel.setFileDate("2016/10/02");
-        dataModel.setFileSize("50mb");
-        dataModel.setFileType("jpg");
-        dataModel.setFileTime("12:29");
-
-        mDatas.add(dataModel);
-
     }
 
     @Nullable
@@ -60,32 +53,96 @@ public class FilesearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Get the intent, verify the action and get the query
         View rootView = inflater.inflate(R.layout.fragment_filesearch, container, false);
-
-        Context context = rootView.getContext();
+        // Recyclerview
+        context = rootView.getContext();
         mRecyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
-        mRecyclerView.setAdapter(mAdapter = new FileSearchAdapter(context, mDatas));
-        //To define new OptionMenu for fragment.
+        mLinearLayoutManager = new LinearLayoutManager(context);
+        mRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mAdapter = new FileSearchAdapter(context);
+        mRecyclerView.setAdapter(mAdapter);
+        // To define new OptionMenu for fragment.
         setHasOptionsMenu(true);
-
-        int position = getArguments().getInt("position");
-        Intent intent = getActivity().getIntent();
-        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-            String query = intent.getStringExtra(SearchManager.QUERY);
-            Log.d(TAG, query);
-//            doMySearch(query);
-        }
         return rootView;
     }
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.options_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.search);
+        final SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         // Associate searchable configuration with the SearchView
         SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
         searchView.setIconifiedByDefault(false);
-        Log.d(TAG, "" + searchManager.getSearchableInfo(getActivity().getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                SearchTask mTask = new SearchTask(context);
+                mTask.execute(query);
+                // close search view, IME
+                searchItem.collapseActionView();
+                searchView.clearFocus();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, newText);
+                return false;
+            }
+        });
         super.onCreateOptionsMenu(menu, inflater);
     }
+
+    private class SearchTask extends AsyncTask<String, Void, ArrayList<String>> {
+        private ProgressDialog pr_dialog;
+        private final Context context;
+
+        private SearchTask(Context c) {
+            context = c;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            mAdapter.clearContent();
+            pr_dialog = ProgressDialog.show(context, null,
+                    getString(R.string.search));
+            pr_dialog.setCanceledOnTouchOutside(false);
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+            String location = Environment.getExternalStorageDirectory().toString();
+            Log.d(TAG, params.toString());
+            return SimpleUtils.searchInDirectory(location, params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(final ArrayList<String> files) {
+            int len = files != null ? files.size() : 0;
+            mDatas = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                updateData(files.get(i));
+            }
+            mAdapter.addContent(mDatas);
+            pr_dialog.dismiss();
+        }
+    }
+
+    protected void updateData(String filepath) {
+        FileSearchModel dataModel = new FileSearchModel();
+        File file = new File(filepath);
+        Date lastModate = new Date(file.lastModified());
+
+        dataModel.setFileName(file);
+        dataModel.setFileType(filepath.substring(filepath.lastIndexOf(".") + 1));
+        dataModel.setFileSize((file.length()));
+        dataModel.setFileDate(lastModate);
+        dataModel.setFileTime(lastModate);
+
+        mDatas.add(dataModel);
+
+    }
+
 }
+
